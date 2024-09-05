39df1ed77784783e62d4ba131e80409600d4cda8
Souvik Pramanik <54247555+Souvik-Pramanik@users.noreply.github.com>
2024-09-05 13:04:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
+const port = 3000;
 app.use(cors());
-
+app.get('/api/quote', async (req, res) => {
+  try {
+    const response = await axios.get('https://favqs.com/api/qotd', {
+      headers: {
+        'Authorization': 'Bearer d9efc5c19c4876b2ee43d19af4dd2c46'
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching quote', error: error.message });
+  }
+});
 // Route for fetching quotes by author
 app.get('/api/quote/search', async (req, res) => {
   const author = req.query.author;
